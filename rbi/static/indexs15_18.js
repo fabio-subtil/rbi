c1a1e0af0e3a609db8d4da4a0a35e27d977a936a
fabio-subtil <FABIO.SUBTIL@OUTLOOK.COM>
2024-10-22 17:12:26 -0300

    Alterações HTML

--- rbi/static/indexs15_18.js
+++ rbi/static/indexs15_18.js
@@ -1,7 +1,5 @@
 static/indexs15_18.js
 
-
-
 var toggler = document.getElementsByClassName("caret");
 var i;
 
@@ -15,10 +13,11 @@ for (i = 0; i < toggler.length; i++) {
 function calcular() {
     var num1 = Number(document.getElementById("compsemcostura").value);
     var num2 = Number(document.getElementById("monitora").value);
-    var num2 = Number(document.getElementById("tipoisol").value);
-    var num2 = Number(document.getElementById("qualipint").value);
-    var num2 = Number(document.getElementById("projetocont").value);
-    var num2 = Number(document.getElementById("interface").value);
-    var num2 = Number(document.getElementById("datapint").value);
-
+    var num3 = Number(document.getElementById("tipoisol").value);
+    var num4 = Number(document.getElementById("qualipint").value);
+    var num5 = Number(document.getElementById("projetocont").value);
+    var num6 = Number(document.getElementById("interface").value);
+    var num7 = Number(document.getElementById("datapint").value);
+    var num8 = Number(document.getElementById("driver").value);
+    
 }
